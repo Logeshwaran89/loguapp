aaaad05eac2cc7a8d0e674000c2a8172a1978ba1
Jaikumar <jaikumar.vajravel@pulsesync.com>
2020-10-03 13:02:52 +0800

    Added image to carousel

--- src/DemoCarousel.js
+++ src/DemoCarousel.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import vvv from "./vvv.jpg"
+
 
 
 let styles = {
@@ -13,7 +15,7 @@ function DemoCarousel() {
 	<div style={styles}>
 		<Carousel>
 			<div>
-				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
+				<img src={vvv} alt="Hong Kong" />
 				<p className="legend">Hong Kong</p>
 			</div>
 			<div>
