337fb6f60570597f8d1a3641144283b9fa50be92
logeshwaran <vlogu89@gmail.com>
2020-10-03 09:44:53 +0530

    https://github.com/Logeshwaran89/loguapp.git

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import clock from "./clock";
 import calculator from "./calculator";
 import maps from "./maps";
+import DemoCarousel from "./DemoCarousel"
 import './Appd.css';
  
 //This is logu
@@ -20,12 +21,13 @@ class App extends Component {
           <li><NavLink to="/clock">clock</NavLink></li>
           <li><NavLink exact to="/">calculator</NavLink></li>
             <li><NavLink to="/maps">maps</NavLink></li>
+            <li><NavLink to="/DemoCarousel">DemoCarousel</NavLink></li>
           </ul>
           <div className="content">
           <Route path="/clock" component={clock}/>
             <Route exact path="/" component={calculator}/>
             <Route path="/maps" component={maps}/>
-             
+            <Route path="/DemoCarousel" component={DemoCarousel}/> 
           </div>
         </div>
           </HashRouter>
